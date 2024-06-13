9eb9da14a1e33eba722e849300f8df2020562af0
fakys <fakysgg@gmail.com>
2024-06-13 19:31:58 +0300

    create style add objects

--- resources/views/admin/add_object.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('admin.layouts.base')
-
-@section('content')
-    <div class="container">
-        @if($table = 'users')
-            <x-user-form-components/>
-        @endif
-    </div>
-@endsection
---
+++ resources/views/admin/create_object.blade.php
@@ -0,0 +1,22 @@
+@extends('admin.layouts.base')
+
+@section('content')
+    <div class="container">
+        <form method="post" action="{{route('admin.add_object', ['table'=>$table])}}">
+        @if($table == 'users')
+            <x-users-form-component/>
+        @elseif($table =='categories')
+            <x-category-form-component/>
+        @elseif($table =='genres')
+            <x-genre-form-component/>
+        @elseif($table =='tags')
+            <x-tag-form-component/>
+        @elseif($table =='authors')
+            <x-author-form-component/>
+        @elseif($table =='users_groups')
+            <x-user-group-form-component/>
+        @endif
+            <div><input type="submit" class="btn btn-primary" value="Создать"></div>
+        </form>
+    </div>
+@endsection
